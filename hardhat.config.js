7d7053d5269512ab08948624190303245c1427c3
bvenkatanaidu316 <110693409+bvenkatanaidu316@users.noreply.github.com>
2022-09-04 09:49:45 +0530

    Add files via upload

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,16 +1,30 @@
 require("@nomiclabs/hardhat-waffle");
+
+// This is a sample Hardhat task. To learn how to create your own go to
+// https://hardhat.org/guides/create-task.html
+task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+  const accounts = await hre.ethers.getSigners();
+
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
+
+require("@nomiclabs/hardhat-waffle");
+// Import and configure dotenv
 require("dotenv").config();
 
 module.exports = {
   solidity: "0.8.0",
   networks: {
     rinkeby: {
-      url: process.env.STAGING_QUICKNODE_KEY,
+      // This value will be replaced on runtime
+      url: process.env.STAGING_ALCHEMY_KEY,
       accounts: [process.env.PRIVATE_KEY],
     },
     mainnet: {
       chainId: 1,
-      url: process.env.PROD_QUICKNODE_KEY,
+      url: process.env.PROD_ALCHEMY_KEY,
       accounts: [process.env.PRIVATE_KEY],
     },
   },
